fa1a33fb615f58491cfb0af589ad0d86005684c4
puranach <puranach@tbi-worker.inet.dkfz-heidelberg.de>
2018-03-26 17:06:51 +0200

    tset

--- work/d0/a76500fc552b5faab268053bc5286f/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-mkdir genomeDir
-STAR --runThreadN 1 		 --runMode genomeGenerate 		 --genomeDir genomeDir 		 --genomeFastaFiles genome.fa 		 --sjdbGTFfile annotation.gtf 		 --sjdbOverhang 100
