17ba78100d8cfbe41faa3c28e3de4c5c0d2571a0
puranach <puranach@tbi-worker.inet.dkfz-heidelberg.de>
2018-03-26 17:13:07 +0200

    t

--- establish_and_test.sh
+++ establish_and_test.sh
@@ -13,7 +13,9 @@ nextflow run -without-docker -w work grape-nf --index /home/puranach/envs/grape-
 --annotation /home/puranach/envs/grape-nf3/share/nextflow/assets/guigolab/grape-nf/data/annotation.gtf
 
 N E X T F L O W  ~  version 0.28.0
-##Launching `guigolab/grape-nf` [elated_shockley] - revision: 75ffd8da0a [master]
+
+N E X T F L O W  ~  version 0.28.0
+##Launching `guigolab/grape-nf` [zen_edison] - revision: 75ffd8da0a [master]
 ##
 ##G R A P E ~ RNA-seq Pipeline
 ##
@@ -53,19 +55,19 @@ N E X T F L O W  ~  version 0.28.0
 ##Merging                         : none
 ##
 ##===============
-##Output files db -> /abi/data/puranach/packages/test_grapenf3/work/pipeline.db
+##Output files db -> /abi/data/puranach/packages/test_grape/pipeline.db
 ##===============
 ##
 ##[warm up] executor > local
-##[e9/cac669] Submitted process > t_index (genome-RSEM-1.2.21)
-##[06/36c5e2] Submitted process > fastaIndex (genome-SAMtools-0.1.19)
-##[e1/9581fc] Submitted process > index (genome-STAR-2.4.0j)
-##[a8/121b6c] Submitted process > mapping (test1-STAR-2.4.0j)
-##[5a/a1bbbf] Submitted process > inferExp (test1-RSeQC-2.3.9)
-##[da/9211ae] Submitted process > quantification (test1-RSEM-1.2.21)
-##[c1/f6ef78] Submitted process > contig (test1-RGCRG-0.1)
-##[ad/88023d] Submitted process > bigwig (test1-STAR-2.4.0j)
-##[da/9211ae] NOTE: Process `quantification (test1-RSEM-1.2.21)` terminated with an error exit status (127) -- Error is ignored
+##[08/df89c9] Submitted process > index (genome-STAR-2.4.0j)
+##[d1/52dfd2] Submitted process > fastaIndex (genome-SAMtools-0.1.19)
+##[57/cff50d] Submitted process > t_index (genome-RSEM-1.2.21)
+##[58/69dc3a] Submitted process > mapping (test1-STAR-2.4.0j)
+##[be/aedb4b] Submitted process > inferExp (test1-RSeQC-2.3.9)
+##[85/aa6c51] Submitted process > contig (test1-RGCRG-0.1)
+##[d2/273a66] Submitted process > quantification (test1-RSEM-1.2.21)
+##[7a/738678] Submitted process > bigwig (test1-STAR-2.4.0j)
+##[d2/273a66] NOTE: Process `quantification (test1-RSEM-1.2.21)` terminated with an error exit status (127) -- Error is ignored
 ##
 ##-----------------------
 ##Pipeline run completed.
